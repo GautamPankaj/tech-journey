0bc7eb365db29dd15e4c1b2752cac0610e98c590
GautamPankaj <gautam.pankaj87@gmail.com>
2022-05-21 15:45:31 +0530

    Commit

---
+++ SelfPacedDSA/src/goal/mathematics/CountDigit.java
@@ -0,0 +1,33 @@
+package goal.mathematics;
+
+public class CountDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int x = 923;
+		int count = countDigit(x);
+		System.out.println("total digit " + count);
+		
+		int countRec = countDigitRec(x);
+		System.out.println("total digit rec is  " + countRec);
+	}
+
+	private static int countDigitRec(int x) {
+		// TODO Auto-generated method stub
+		if(x == 0)
+			return 0;
+		return 1 + countDigitRec(x/10);
+	}
+
+	private static int countDigit(int x) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		while (x > 0) {
+			x = x/10;
+			count++;
+		}
+		return count;
+	}
+
+}
