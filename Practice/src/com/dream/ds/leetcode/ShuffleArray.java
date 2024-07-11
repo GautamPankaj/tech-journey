85f1256bafa72d4fb8e39e1b7e995e418468625f
GautamPankaj <gautam.pankaj87@gmail.com>
2024-07-11 23:25:32 +0530

    Create ShuffleArray.java

---
+++ Practice/src/com/dream/ds/leetcode/ShuffleArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            result[j] = nums[i];
+            result[++j] = nums[n+i];
+            j++;
+        }
+        return result;
+    }
+}
