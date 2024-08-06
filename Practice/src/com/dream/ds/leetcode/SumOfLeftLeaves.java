31c3a05a76d5abb6faaafd3540e47a4ef8e494f9
GautamPankaj <gautam.pankaj87@gmail.com>
2024-08-07 00:10:45 +0530

    Create SumOfLeftLeaves.java

---
+++ Practice/src/com/dream/ds/leetcode/SumOfLeftLeaves.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        int sum=0;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            if(temp.left != null && temp.left.left == null && temp.left.right == null)
+                sum = sum + temp.left.val;
+            if(temp.left != null)
+                queue.add(temp.left);
+            if(temp.right != null)
+                queue.add(temp.right);
+        }
+        return sum;
+    }
+}
